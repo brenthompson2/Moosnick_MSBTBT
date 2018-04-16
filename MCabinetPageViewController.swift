163cb8ed962457910ac72f3f16892e3af22139fb
Brendan Thompson <brenthompson2@gmail.com>
2018-04-16 14:00:33 -0400

    Researched CabinetPageView DataSource Solutions

--- MCabinetPageViewController.swift
+++ MCabinetPageViewController.swift
@@ -9,31 +9,32 @@
 import UIKit
 
 class MCabinetPageViewController: UIPageViewController {
+    
+    var numPages: Int = 1
+    var cabinetControllers = [UINavigationController]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.isDoubleSided = false
         
         // The class for the data source is not important, but it must implement the
         let storyBoard = UIStoryboard(name: "Main", bundle: nil)
-        var tempViewControllers = [UINavigationController]()
         
         // ======================================
         // Create Pages
         // ======================================
-        let numPages = 5
         
 //        for _ in 0..<numPages{
             // Instantiate a Page
             var tempNavController = storyBoard.instantiateViewController(withIdentifier: "navForCabinet") as! UINavigationController
             var tempViewController = tempNavController.topViewController! as! MCabinetViewController
             tempViewController.dataSource = MCabinetCollectionDataSource()
-            tempViewControllers.append(tempNavController)
+            cabinetControllers.append(tempNavController)
+            setViewControllers(cabinetControllers, direction: UIPageViewControllerNavigationDirection.forward, animated: true, completion: nil)
             
 //        }
-        
-        setViewControllers(tempViewControllers, direction: UIPageViewControllerNavigationDirection.forward, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -53,3 +54,39 @@ class MCabinetPageViewController: UIPageViewController {
     */
 
 }
+
+// Example PageViewController = https://gist.github.com/andreif/3bace9961c7e70995856
+//class MCabinetPageViewDataSource: UIPageViewControllerDataSource {
+//    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+//        let vc = viewController as! MCabinetViewController
+//        var index = vc.pageIndex as Int
+//        if (index == 0 || index == NSNotFound) {
+//            return nil
+//        }
+//        index--
+//        return self.viewControllerAtIndex(index)
+//    }
+//
+//    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+//        let vc = viewController as! MCabinetViewController
+//        var index = vc.pageIndex as Int
+//        if (index == NSNotFound) {
+//            return nil
+//        }
+//        index++
+//        if (index == PAGES.count) {
+//            return nil
+//        }
+//        return self.viewControllerAtIndex(index)
+//    }
+//
+//    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+//        return PAGES.count
+//    }
+//
+//    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+//        return 0
+//    }
+//
+//}
+
